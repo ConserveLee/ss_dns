6fcfcc81b40ea3ac715120a5312314c103a1d4d4
leeli <leeli@xiaoe-tech.com>
2020-03-05 10:45:25 +0800

    测试下

--- main.go
+++ main.go
@@ -90,18 +90,16 @@ func getIP() (string, bool) {
 	bytes, _ := ioutil.ReadAll(resp.Body)
 	bLen 	 := len(bytes)
 
-	fmt.Println(string(bytes))
-	fmt.Println(bLen)
 	/** 返回体长度出错 */
-	//if bLen < 7 {return dnsError, false}
-	///** 只有一个IP，直接返回 */
-	//if bLen < 14 {return string(bytes), true}
-	///** 获取第一个IP */
-	//for i := 0; i < bLen; i++ {
-	//	if bytes[i] == 59 {return string(bytes[:i]), true}
-	//	/** 返回体长度出错 */
-	//	if i > 15 {return dnsError, false}
-	//}
+	if bLen < 8 {return dnsError, false}
+	/** 只有一个IP，直接返回 */
+	if bLen < 15 {return string(bytes), true}
+	/** 获取第一个IP */
+	for i := 0; i < bLen; i++ {
+		if bytes[i] == 59 {return string(bytes[:i]), true}
+		/** 返回体长度出错 */
+		if i > 15 {return dnsError, false}
+	}
 	return "", false
 }
 
